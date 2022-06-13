a1f5d51e7cbbe3c17b65acee5e3e01cfb012933e
Matt Venn <matt@mattvenn.net>
2022-06-13 10:40:47 +0200

    initial commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+fetch:
+	curl https://wokwi.com/api/projects/334295537442357843/verilog > src/wokwi.v
+
+# needs PDK_ROOT and OPENLANE_ROOT, OPENLANE_IMAGE_NAME set from your environment
+harden:
+	docker run --rm \
+	-v $(OPENLANE_ROOT):/openlane \
+	-v $(PDK_ROOT):$(PDK_ROOT) \
+	-v $(CURDIR):/work \
+	-e PDK_ROOT=$(PDK_ROOT) \
+	-u $(shell id -u $(USER)):$(shell id -g $(USER)) \
+	$(OPENLANE_IMAGE_NAME) \
+	/bin/bash -c "./flow.tcl -overwrite -design /work/src -run_path /work/runs -tag adder"
+
+.PHONY: clean harden
---
+++ src/config.tcl
@@ -0,0 +1,28 @@
+# User config
+set script_dir [file dirname [file normalize [info script]]]
+
+set ::env(DESIGN_NAME) wokwi
+
+# save some time
+set ::env(RUN_KLAYOUT_XOR) 0
+set ::env(RUN_KLAYOUT_DRC) 0
+
+# don't put clock buffers on the outputs, need tristates to be the final cells
+set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
+
+# Change if needed
+set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/src/wokwi.v"
+
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 70 70"
+set ::env(PL_TARGET_DENSITY) 0.75
+
+# Fill this
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "clk"
+
+set ::env(DESIGN_IS_CORE) 0
+set ::env(RT_MAX_LAYER) {met4}
+
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
