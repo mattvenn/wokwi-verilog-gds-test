0b23759858af0f30497136d77db351697ed54724
Matt Venn <matt@mattvenn.net>
2022-08-05 12:03:48 +0200

    use alterative li cells

--- template/config.tcl
+++ template/config.tcl
@@ -17,16 +17,27 @@ set ::env(SYNTH_READ_BLACKBOX_LIB) 1
 # read all verilog files
 set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/*.v]
 
+# absolute die size
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 100 100"
 set ::env(FP_CORE_UTIL) 45
 
-# Fill this
+# use alternative efabless decap cells to solve LI density issue
+set ::env(DECAP_CELL) "\
+    sky130_fd_sc_hd__decap_3 \
+    sky130_fd_sc_hd__decap_4 \
+    sky130_fd_sc_hd__decap_6 \
+    sky130_fd_sc_hd__decap_8 \
+    sky130_ef_sc_hd__decap_12"
+
+# clock
 set ::env(CLOCK_PERIOD) "100"
 set ::env(CLOCK_PORT) "clk"
 
+# don't use power rings or met5
 set ::env(DESIGN_IS_CORE) 0
 set ::env(RT_MAX_LAYER) {met4}
 
+# connect to first digital rails
 set ::env(VDD_NETS) [list {vccd1}]
 set ::env(GND_NETS) [list {vssd1}]
