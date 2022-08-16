f94406845be6db7b95c37f2a02ccf1b152443f5b
Omer Kilic <omerkilic@gmail.com>
2022-08-16 17:53:32 +0100

    general fixup, tb now works

--- src/user_module_339898704941023827.v
+++ src/user_module_339898704941023827.v
@@ -1,11 +1,19 @@
 `default_nettype none
 
 module user_module_339898704941023827(
-  //input CLK, // for testing with real hardware
-
-  input [7:0] io_in,
+  //input TINY_CLK, // XXX: for testing with real hardware
+  input [7:0] io_in, //using io_in[0] as clk, io_in[1] as reset
   output [7:0] io_out
 );
+
+  wire clk;
+  //assign clk = TINY_CLK, // XXX: for testing with real hardware
+  assign clk = io_in[0];
+  wire reset;
+  assign reset = io_in[1];
+
+  reg [23:0] counter = 0;
+  reg [3:0] state = 0;
 
   // patterns for common anode wiring
                        //76543210
@@ -19,34 +27,35 @@ module user_module_339898704941023827(
   reg[7:0] letter_i = 8'b11001111;
   reg[7:0] letter_c = 8'b11000110;
   reg[7:0] letter_blank =  8'b11111111;
-
-  reg [23:0] counter;
-  reg [3:0] state;
-
-  reg [7:0] led_out;
-
-  //always @(posedge CLK) begin
-  always @(posedge io_in[0]) begin
-    counter <= counter + 1;
-
-    if (counter[23])
-      state <= state + 1;
+  
+  reg [7:0] led_out = 0;
+  
+  always @(posedge clk) begin
+    if (reset) begin
+        counter <= 0;
+        state <= 0;
+    end else begin
+      counter <= counter + 1;
+      if (counter[23])
+        state <= state + 1;
+    end
 
     case(state)
-      4'b0000 : led_out = letter_h;
-      4'b0001 : led_out = letter_e;
-      4'b0010 : led_out = letter_l;
-      4'b0011 : led_out = letter_l;
-      4'b0100 : led_out = letter_o;
-      4'b0101 : led_out = letter_blank;
-      4'b0110 : led_out = letter_a;
-      4'b0111 : led_out = letter_s;
-      4'b1000 : led_out = letter_i;
-      4'b1001 : led_out = letter_c;
-      4'b1010 : led_out = letter_blank;
-      4'b1011 : led_out = letter_blank;
-      //4'b1100 : state = 0; // this somehow breaks things?
-      default : led_out = letter_blank;
+      4'b0000 : led_out <= letter_h;
+      4'b0001 : led_out <= letter_e;
+      4'b0010 : led_out <= letter_l;
+      4'b0011 : led_out <= letter_l;
+      4'b0100 : led_out <= letter_o;
+      4'b0101 : led_out <= letter_blank;
+      4'b0110 : led_out <= letter_a;
+      4'b0111 : led_out <= letter_s;
+      4'b1000 : led_out <= letter_i;
+      4'b1001 : led_out <= letter_c;
+      4'b1010 : led_out <= letter_blank;
+      4'b1011 : led_out <= letter_blank;
+      4'b1100 : state <= 0; // reset
+
+      default : led_out <= letter_blank;
     endcase
   end
 
--- src/user_module_339898704941023827_tb.v
+++ src/user_module_339898704941023827_tb.v
@@ -6,16 +6,7 @@ module user_module_339898704941023827_tb;
 reg [7:0] io_in;
 wire [7:0] io_out;
 
-reg [23:0] counter;
-reg [3:0] state;
-
-parameter CLK_HALF_PERIOD = 5;
-
-initial begin
-  io_in[0] = 0;
-  #(CLK_HALF_PERIOD);
-  forever io_in[0] = #(CLK_HALF_PERIOD) ~io_in[0];
-end
+user_module_339898704941023827 UUT (.io_in(io_in), .io_out(io_out));
 
 initial begin
   $dumpfile("user_module_339898704941023827_tb.vcd");
@@ -23,9 +14,25 @@ initial begin
 end
 
 initial begin
-   #1_000_000; // Wait a long time in simulation units (adjust as needed).
+   #100_000_000; // Wait a long time in simulation units (adjust as needed).
    $display("Caught by trap");
    $finish;
  end
+
+parameter CLK_HALF_PERIOD = 5;
+always begin
+  io_in[0] = 1'b1;
+  #(CLK_HALF_PERIOD);
+  io_in[0] = 1'b0;
+  #(CLK_HALF_PERIOD);
+end
+
+initial 
+begin
+  #20
+	io_in[1] = 1;
+	#(CLK_HALF_PERIOD);
+	io_in[1] = 0;
+end
 
 endmodule
