a7f2b6b003fabedfc9145c478b8b971f723f85f2
Omer Kilic <omerkilic@gmail.com>
2022-08-16 15:35:12 +0100

    specify simulation runtime

--- src/user_module_339898704941023827_tb.v
+++ src/user_module_339898704941023827_tb.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-`include "user_module_339898704941023827.v"
+//`include "user_module_339898704941023827.v"
 
 module user_module_339898704941023827_tb;
 
@@ -11,17 +11,21 @@ reg [3:0] state;
 
 parameter CLK_HALF_PERIOD = 5;
 
-initial
-begin
+initial begin
   io_in[0] = 0;
   #(CLK_HALF_PERIOD);
   forever io_in[0] = #(CLK_HALF_PERIOD) ~io_in[0];
 end
 
-initial
-begin
+initial begin
   $dumpfile("user_module_339898704941023827_tb.vcd");
   $dumpvars(0, user_module_339898704941023827_tb);
 end
+
+initial begin
+   #1_000_000; // Wait a long time in simulation units (adjust as needed).
+   $display("Caught by trap");
+   $finish;
+ end
 
 endmodule
