e03982c492cc3e8fccf055f4a89cd2f5d5e6792c
Omer Kilic <omerkilic@gmail.com>
2022-08-16 17:51:53 +0100

    add our own custom config.tcl

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@ fetch:
 # HACK: we don't need to fetch this as we have our own verilog source, manually created: src/user_module_339898704941023827.v
 #	curl https://wokwi.com/api/projects/$(WOKWI_PROJECT_ID)/verilog > src/user_module_$(WOKWI_PROJECT_ID).v
 	sed -e 's/USER_MODULE_ID/$(WOKWI_PROJECT_ID)/g' template/scan_wrapper.v > src/scan_wrapper_$(WOKWI_PROJECT_ID).v
-	sed -e 's/USER_MODULE_ID/$(WOKWI_PROJECT_ID)/g' template/config.tcl > src/config.tcl
+# HACK: instead of globbing the entire src directory, specify modules explicitly in custom config.tcl
+#	sed -e 's/USER_MODULE_ID/$(WOKWI_PROJECT_ID)/g' template/config.tcl > src/config.tcl
 	echo $(WOKWI_PROJECT_ID) > src/ID
 
 # needs PDK_ROOT and OPENLANE_ROOT, OPENLANE_IMAGE_NAME set from your environment
---
+++ src/config.tcl
@@ -0,0 +1,44 @@
+# User config
+set script_dir [file dirname [file normalize [info script]]]
+
+# HACK: edited this, instead of relying on the Makefile to prep this for us
+set ::env(DESIGN_NAME) scan_wrapper_339898704941023827
+
+# save some time
+set ::env(RUN_KLAYOUT_XOR) 0
+set ::env(RUN_KLAYOUT_DRC) 0
+
+# don't put clock buffers on the outputs
+set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
+
+# allow use of specific sky130 cells
+set ::env(SYNTH_READ_BLACKBOX_LIB) 1
+
+# HACK: explicitly specify which files we are using, ignore TB etc.
+set ::env(VERILOG_FILES) user_module_339898704941023827.v
+
+# absolute die size
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 100 100"
+set ::env(FP_CORE_UTIL) 45
+set ::env(PL_BASIC_PLACEMENT) {1}
+
+# use alternative efabless decap cells to solve LI density issue
+set ::env(DECAP_CELL) "\
+    sky130_fd_sc_hd__decap_3 \
+    sky130_fd_sc_hd__decap_4 \
+    sky130_fd_sc_hd__decap_6 \
+    sky130_fd_sc_hd__decap_8 \
+    sky130_ef_sc_hd__decap_12"
+
+# clock
+set ::env(CLOCK_PERIOD) "100"
+set ::env(CLOCK_PORT) "clk"
+
+# don't use power rings or met5
+set ::env(DESIGN_IS_CORE) 0
+set ::env(RT_MAX_LAYER) {met4}
+
+# connect to first digital rails
+set ::env(VDD_NETS) [list {vccd1}]
+set ::env(GND_NETS) [list {vssd1}]
