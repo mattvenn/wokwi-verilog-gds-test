cef784c0a1021cdbee803760ba577b9bced42e4a
Omer Kilic <omerkilic@gmail.com>
2022-08-17 11:10:28 +0100

    fix clk error for toplevel

--- src/sevenseg_top.v
+++ src/sevenseg_top.v
@@ -7,8 +7,20 @@ module sevenseg_top (
   output [7:0] io_out
 );
 
-assign io_in[0] = TINY_CLK;
+// XXX: All I really want is this: assign io_in[0] = TINY_CLK;
+// But yosys errors out:
+//   Net 'TINY_CLK' is multiply driven by cell port TINY_CLK.O and top level input 'io_in[0]'.
+// hence the extra wire and explicit assignments. There must be a better way?
+wire [7:0] io_in_x;
+assign io_in_x[0] = TINY_CLK;
+assign io_in_x[1] = io_in[1];
+assign io_in_x[2] = io_in[2];
+assign io_in_x[3] = io_in[3];
+assign io_in_x[4] = io_in[4];
+assign io_in_x[5] = io_in[5];
+assign io_in_x[6] = io_in[6];
+assign io_in_x[7] = io_in[7];
 
-user_module_339898704941023827 mod1(.io_in(io_in), .io_out(io_out));
+user_module_339898704941023827 mod1(.io_in(io_in_x), .io_out(io_out));
 
 endmodule
